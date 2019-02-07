3462517ef1bc6511ea81d495724c10c0c690d665
sherruh <sherruh@gmail.com>
2019-02-07 20:32:13 +0600

    Final Commit

--- src/Car.java
+++ src/Car.java
@@ -15,4 +15,20 @@ public class Car {
     public void drive(){
         System.out.println("Drive this car");
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
+        Annotation[] annotations=Car.class.getAnnotations();
+        for(Annotation annotation:annotations){
+            System.out.println("Annotation of class: "+ annotation.toString());
+        }
+
+        Method[] methods=Car.class.getDeclaredMethods();
+        for(Method method:methods){
+            Annotation[] annotationsOfMethod=method.getDeclaredAnnotations();
+            for(Annotation annotationOfMethod: annotationsOfMethod){
+                System.out.println("Annotation of method: "+method.getName()+" is: "+ annotationOfMethod.toString());
+            }
+        }
+
+        Field[] fields=Car.class.getDeclaredFields();
+        for(Field field: fields){
+            Annotation[] annotationsOfFields=field.getAnnotations();
+            for (Annotation annotationOfField: annotationsOfFields){
+                System.out.println("Annotation of field: "+field.getName()+" is: "+annotationOfField.toString());
+            }
+        }
+
+        Method method=Car.class.getDeclaredMethod("drive");
+        method.invoke(new Car("BMW",120));
     }
 }
